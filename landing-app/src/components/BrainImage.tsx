9fc686a3309e769e7b41aff156ca1f268848029c
Ankit <aayanchandra2006@gmail.com>
2025-10-25 19:56:14 -0700

    Merge stashed layout into dashboard; move heatmap into BrainImage; reconcile styles

--- landing-app/src/components/BrainImage.tsx
+++ landing-app/src/components/BrainImage.tsx
@@ -11,7 +11,17 @@ export default function BrainImage({
 }: Props): React.JSX.Element {
   return (
     <div className="card center-image">
-      <img src={src} alt={alt} />
+      <div className="brain-wrap">
+        <img src={src} alt={alt} />
+        <div className="heatmap" aria-hidden="true">
+          <span className="heat-blob red b1"></span>
+          <span className="heat-blob green b2"></span>
+          <span className="heat-blob blue b3"></span>
+          <span className="heat-blob red b4"></span>
+          <span className="heat-blob green b5"></span>
+          <span className="heat-blob blue b6"></span>
+        </div>
+      </div>
     </div>
   )
 }
